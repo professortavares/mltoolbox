ed5502f1d444d21c6137bd2ddcac86617bcfaf8c
Prof. Tavares <leodaniel@gmail.com>
2023-02-05 16:23:05 -0300

    Implementação do histograma

---
+++ mltoolbox/visualization/__init__.py
@@ -0,0 +1,1 @@
+from mltoolbox.visualization.dataviz import DataViz
--- mltoolbox/visualization/dataviz.py
+++ mltoolbox/visualization/dataviz.py
@@ -4,9 +4,84 @@ using matplotlib and seaborn.
 """
 
 import matplotlib.pyplot as plt
+import pandas as pd
 import seaborn as sns
 
 class DataViz:
-    """
+    def __init__(self, figsize=(8, 5)):
+        """
+        Constructor.
 
-    """
+        Parameters:
+        ----------
+        figsize: tuple, default=(8, 5)
+            Figure size.
+        """
+        self.figsize = figsize
+
+    def set_figsize(self, figsize:tuple)->None:
+        """
+        Sets the figure size.
+        :param figsize: tuple
+        :return: None
+        """
+        self.figsize = figsize
+
+    def plot_histogram(self,
+                       df:pd.DataFrame,
+                       column:str,
+                       bins:int=10,
+                       xlabel:str=None,
+                       ylabel:str=None,
+                       title:str=None)->None:
+        """
+        Plots a histogram from a dataframe.
+
+        Parameters:
+        ----------
+
+        df: pd.DataFrame
+            Dataframe to plot histogram from.
+
+        column: str
+            Column to plot histogram from.
+
+        bins: int, default=10
+            Number of bins to use.
+
+        xlabel: str, default=None
+            Label for the x-axis.
+
+        ylabel: str, default=None
+            Label for the y-axis.
+
+        title: str, default=None
+            Title of the plot.
+
+        Returns:
+        -------
+        None
+        """
+
+        # Use seaborn to plot the histogram
+        sns.displot(df,
+                    x=column,
+                    bins=bins,
+                    kde=True,
+                    height=self.figsize[1],
+                    aspect=self.figsize[0]/self.figsize[1])
+
+        # If some of the parameters are not set, set them
+        if xlabel is None:
+            xlabel = column
+        if ylabel is None:
+            ylabel = 'Frequency'
+        if title is None:
+            title = 'Histogram of ' + column
+
+        plt.xlabel(xlabel)
+        plt.ylabel(ylabel)
+        plt.title(title)
+
+        # Show the plot
+        plt.show()
